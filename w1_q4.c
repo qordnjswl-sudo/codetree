7dfaf5a02a26ad912dedaaf649fb4af2eca5cc8c
qordnjswl-sudo <qordnjswl@hanyang.ac.kr>
2026-03-08 16:34:17 +0900

    Add w1_q4.c to print float and int values

---
+++ w1_q4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	float val = 123.456;
+	int valu = 89;
+	printf("float value = %f \n", val);
+	printf("int value = %d \n", valu);
+	printf("Product between float and int: %f", val*valu);
+	return 0;
+}
