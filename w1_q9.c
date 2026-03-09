69ea88116b4522d4d62ed5cf9880075c8ac9afc6
qordnjswl-sudo <qordnjswl@hanyang.ac.kr>
2026-03-09 18:33:44 +0900

    Create w1_q9.c

---
+++ w1_q9.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	char fir, sec;
+	printf("_ A R _");
+	printf("Enter any character to make a work that have meaning\n");
+	printf("Enter first and last character: \n");
+	scanf("%c %c", &fir, &sec);
+	
+	printf("This is your word %c A R %c", fir, sec);
+	return 0;
+}
